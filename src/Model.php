f1bfd905522ad897548e7b50e82fcc4c5d7f1fe5
Paweł Firlej <mail@pfco.de>
2016-08-17 18:49:52 +0200

    Feature: import/export Model to/from associative arrays

--- src/Model.php
+++ src/Model.php
@@ -9,5 +9,36 @@ namespace pfcode\MeguminFramework;
  */
 abstract class Model
 {
-    
+    /**
+     * Convert model to associative array
+     * @param int $maxDepth
+     * @return array
+     */
+    public function toAssoc($maxDepth = 10){
+        if(!$maxDepth){
+            return null;
+        }
+
+        $ret = (array) $this;
+
+        foreach($ret as $key => $value){
+            if(is_object($value) && is_subclass_of($value, Model::class)){
+                $ret[$key] = $value->toAssoc($maxDepth - 1);
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Load model parameters from associative array
+     * @param array $assoc
+     */
+    public function fromAssoc(array $assoc){
+        foreach($assoc as $key => $value){
+            if(property_exists($this, $key)){
+                $this->$key = $value;
+            }
+        }
+    }
 }
